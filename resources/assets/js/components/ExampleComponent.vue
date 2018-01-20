6206b1baa38d53958d3754557d44d8353c7ca244
Islam Dudaev <islamwolf23@gmail.com>
2018-01-20 23:02:26 +0000

    Adding video upload function

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -3,10 +3,11 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Upload</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+
+                        ...
                     </div>
                 </div>
             </div>
